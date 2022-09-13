667910d8f69a6da1667efe666b3a40ad472e81ab
Kelly Atmore <kelly.atmore47@gmail.com>
2022-09-12 18:16:50 -0700

    start api request

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import axios from "axios";
+
 function App() {
+  const quote = () => {
+    axios.get("https://api.quotable.io/random").then(function (response) {
+      // handle success
+      console.log(response.data.content);
+    });
+  };
+
+  quote();
+
   return (
     <div className="App">
       <div className="header">
         <p>Quote Machine</p>
       </div>
-      <div className="quote-box"></div>
+      <div className="quote-box">
+        <p className="quote-text">This is a quote</p>
+      </div>
     </div>
   );
 }
