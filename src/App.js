da1cccf75153092a543284bd991dca9e498e2c70
Kelly Atmore <kelly.atmore47@gmail.com>
2022-09-14 16:56:27 -0700

    display quote, add button to change quote

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [quote, setQuote] = useState("this is intitial quote");
+
+  const changeQuote = () => {
+    axios
+      .get("https://api.quotable.io/random")
+      .then((res) => {
+        const data = res.data.content;
+        setQuote(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="App">
       <div className="header">
         <p>Quote Machine</p>
       </div>
       <div className="quote-box">
-        <p className="quote-text">This is a quote</p>
+        <p className="quote-text">{quote}</p>
       </div>
+      <button onClick={() => changeQuote()}>change quote</button>
     </div>
   );
 }
